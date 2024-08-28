ee11a346571b671c0a41fdb3aa9565fbd2b20fe4
chanu <cw8881@naver.com>
2024-08-29 02:39:23 +0900

    [add] : Constant Message object로 string관리

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -1,2 +1,11 @@
 package org.sessac.util
 
+object Message {
+    const val CHOOSE_MENU = "메뉴를 선택해 주세요"
+    const val MOVIE_BOOKING = "영화 예매를 하시겠습니까?"
+    const val SHOW_MOVIE_LIST = "현재 상영 영화 목록 입니다."
+    const val SHOW_THEATER_INFO = "상영관 정보를 확인합니다."
+    const val MOVIE_BOOKING_INFO = "예매 내역을 확인하시겠습니까?."
+    const val SHOW_BOOKING_INFO = "현재 예매 내역 입니다."
+    const val ERROR_INPUT_NUM = "잘못된 입력입니다. 숫자를 입력해 주세요"
+}
