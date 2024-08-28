91ff32f4fa4f89096ae006cfd34d291682540856
chanu <cw8881@naver.com>
2024-08-28 19:34:54 +0900

    [feat] : console util object 구현

---
+++ src/main/kotlin/util/console/Console.kt
@@ -0,0 +1,15 @@
+package util.console
+
+import java.util.*
+
+object Console {
+    private val scanner: Scanner by lazy {
+        Scanner(System.`in`)
+    }
+
+    fun readLine(): String = scanner.nextLine()
+
+    fun close() {
+        scanner.close()
+    }
+}
