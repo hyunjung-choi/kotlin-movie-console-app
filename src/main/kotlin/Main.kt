564c840ea41a63f64933664aeef5b4c22d834b19
chanu <cw8881@naver.com>
2024-08-29 12:41:03 +0900

    [feat] : controller 의존성 외부 주입

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,22 @@
 package org.sessac
 
+import data.repository.CheckBookingInfoAction
+import data.repository.MovieBookingAction
+import data.repository.User
+import org.sessac.ui.InputView
+import org.sessac.ui.OutputView
+import org.sessac.ui.controller.Controller
+import util.console.Console
+
 fun main() {
+    val user = User("새싹")
+
+    Controller(
+        inputView = InputView(),
+        outputView = OutputView(),
+        movieBookingAction = MovieBookingAction(user),
+        checkBookingInfoAction = CheckBookingInfoAction(user),
+    ).selectMenu()
+
+    Console.close()
 }
