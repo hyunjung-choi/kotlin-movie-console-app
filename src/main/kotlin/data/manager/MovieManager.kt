0a1f4bb259a4fb37e5837e6329511930e4953625
chanu <cw8881@naver.com>
2024-09-01 22:25:31 +0900

    [move] : FileManager datasource로 이동

--- src/main/kotlin/data/manager/MovieManager.kt
+++ src/main/kotlin/data/manager/MovieManager.kt
@@ -1,7 +1,7 @@
 package org.sessac.data.manager
 
-import data.manager.FileManager
 import domain.repository.MovieAction
+import org.sessac.data.datasource.FileManager
 import org.sessac.data.model.Seat
 import org.sessac.data.model.SeatStatus
 import org.sessac.utils.ArtUtils
