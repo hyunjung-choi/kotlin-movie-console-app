2b46e126b94756eb382fdbaad009099727af1e4f
chanu <cw8881@naver.com>
2024-09-01 22:29:59 +0900

    [feat] : controller 구현, ui 관련 로직 담당

--- src/main/kotlin/ui/controller/Controller.kt
+++ src/main/kotlin/ui/controller/Controller.kt
@@ -1,41 +1,61 @@
 package org.sessac.ui.controller
 
-import domain.repository.MenuAction
+import domain.model.MovieInfo
+import kotlinx.coroutines.runBlocking
+import org.sessac.domain.usecase.MovieUseCase
 import org.sessac.ui.InputView
 import org.sessac.ui.OutputView
-import org.sessac.utils.Message
-import org.sessac.utils.type.ConsoleType
 
 class Controller(
     private val inputView: InputView,
     private val outputView: OutputView,
-    private val movieBookingAction: MenuAction,  // 영화 예매 동작
-    private val checkBookingInfoAction: MenuAction  // 예약 정보 조회 동작
+    private val movieUseCase: MovieUseCase
 ) {
-    init {
-        outputView.showMenu()
-    }
-
-    fun selectMenu() {
+    fun start() = runBlocking {
+        outputView.printCinemaArt()
         while (true) {
-            if (checkMenu()) break
+            outputView.showMenu()
+            when (inputView.chooseMenu()) {
+                "1" -> handleMovieBooking()
+                "2" -> getBookingInfo()
+                "0" -> break
+                else -> outputView.showInvalidInputMessage()
+            }
         }
     }
 
-    private fun checkMenu(): Boolean = when (inputView.chooseMenu()) {
-        (ConsoleType.MOVIE_BOOKING.menuNum).toString() -> {
-            movieBookingAction.execute()
-            true
-        }
+    private suspend fun handleMovieBooking() {
+        val movieInfos = movieUseCase.getAllMovies()
+        val movieIndex = selectMovie(movieInfos)
+        if (movieIndex == -1) return
 
-        (ConsoleType.CHECK_BOOKING_INFO.menuNum).toString() -> {
-            checkBookingInfoAction.execute()
-            true
-        }
+        val movieInfo = movieInfos[movieIndex]
+        val movieManager = movieUseCase.loadMovie(movieInfo)
 
-        else -> {
-            println(Message.ERROR_INPUT_NUM)
-            false
-        }
+        outputView.printSeatArt()
+        movieUseCase.displaySeats(movieManager)
+
+        val row = inputView.getRowNumber() - 1
+        val col = inputView.getColumnNumber() - 1
+        val success = movieUseCase.bookSeat(movieManager, row, col)
+
+        movieUseCase.displaySeats(movieManager)
+        if (success) {
+            outputView.showBookingSuccess()
+            movieUseCase.saveBookingInfo(
+                MovieInfo(movieInfo.title, movieInfo.emoji, row + 1, col + 1)
+            )
+        } else outputView.showBookingFailure()
+    }
+
+    private suspend fun getBookingInfo() {
+        val bookingInfo = movieUseCase.getBookingInfo()
+        outputView.showBookingInfo(bookingInfo)
+    }
+
+    private fun selectMovie(movieInfos: List<MovieInfo>): Int {
+        outputView.showMovieList(movieInfos.map { it.emoji + it.title })
+        val choice = inputView.getMovieNumber() - 1 // 사용자 입력을 1부터 시작한다고 가정
+        return if (choice in movieInfos.indices) choice else -1
     }
 }
