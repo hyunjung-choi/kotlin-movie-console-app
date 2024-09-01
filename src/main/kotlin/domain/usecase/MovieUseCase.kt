fa1ec9296e47e6725332102cb69f050ff2e7721b
chanu <cw8881@naver.com>
2024-09-01 22:28:40 +0900

    [feat] : movie usecase 구현

--- src/main/kotlin/domain/usecase/MovieUseCase.kt
+++ src/main/kotlin/domain/usecase/MovieUseCase.kt
@@ -1,15 +1,45 @@
 package org.sessac.domain.usecase
 
+import data.datasourceimpl.MovieManager
 import domain.model.MovieInfo
-import org.sessac.data.model.MovieManager
-import org.sessac.data.repository.MovieRepository
+import domain.repository.MovieRepository
 
-class MovieUseCase(private val repository: MovieRepository) {
-    fun saveMovie(movieManager: MovieManager) {
-        repository.saveMovie(movieManager)
+class MovieUseCase(
+    private val movieRepository: MovieRepository
+) {
+    fun getAllMovies(): List<MovieInfo> = movieRepository.getAllMovies()
+
+    fun displaySeats(movieManager: MovieManager) {
+        movieManager.displaySeats()
     }
 
-    fun loadMovie(movieInfo: MovieInfo): MovieManager {
-        return repository.loadMovie(movieInfo)
+    suspend fun bookSeat(movieManager: MovieManager, row: Int, col: Int): Boolean {
+        val success = movieManager.bookSeat(row, col) // 좌석 예매 시도
+        if (movieManager.bookSeat(row, col)) {
+            saveMovie(movieManager) // 예매 성공 시 데이터 저장
+        }
+        return success
+    }
+
+    suspend fun checkBookingInfo(movieInfo: MovieInfo): MovieManager {
+        return movieRepository.loadMovie(movieInfo)
+    }
+
+    fun displaySeatArt(movieManager: MovieManager) {
+        movieManager.getSeatArt()
+    }
+
+    suspend fun loadMovie(movieInfo: MovieInfo): MovieManager {
+        return movieRepository.loadMovie(movieInfo)
+    }
+
+    private suspend fun saveMovie(movieManager: MovieManager) {
+        movieRepository.saveMovie(movieManager)
+    }
+
+    suspend fun getBookingInfo(): List<String> = movieRepository.getBookingInfo()
+
+    suspend fun saveBookingInfo(movieInfo: MovieInfo) {
+        movieRepository.saveBookingInfo(movieInfo)
     }
 }
